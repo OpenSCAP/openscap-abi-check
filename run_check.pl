51818c42c8d7bfdcfc6a8e0db207145383580948
Jan Černý <jcerny@redhat.com>
2018-07-12 12:51:58 +0200

    Update script with CMake
    In OpenSCAP master branch, we have moved to CMake, which uses
    different commands to build OpenSCAP and has a different directory
    structure. We need to change this script to be able to compare API
    and ABI with master branch.

--- run_check.pl
+++ run_check.pl
@@ -29,18 +29,30 @@ sub build {
 	mkdir $version;
 	chdir "openscap-$version";
 
-	if ($version == "master") {
-		system("./autogen.sh");
+	if ($version == "master" or $version ge "1.3.0") {
+		chdir "build";
+		system("cmake -DCMAKE_INSTALL_PREFIX=$root/$version -DENABLE_SCE=TRUE -DCMAKE_BUILD_TYPE=Debug ..");
+		system("make");
+		system("make install");
+		chdir "..";
 	}
-	system("./configure --prefix $root/$version --enable-sce CFLAGS='-g -Og -fpermissive -w'; make install" );
+	else {
+		system("./configure --prefix $root/$version --enable-sce CFLAGS='-g -Og -fpermissive -w'; make install" );
+	}
 
 	chdir "..";
 }
 
 sub dump_abi {
 	my $version = shift;
+	my $libopenscap_path;
 
-	system ("abi-dumper $version/lib/libopenscap.so -public-headers $version/include/openscap -lver $version -o $version.dump");
+	if ($version == "master" or $version ge "1.3.0") {
+		$libopenscap_path = "$version/lib64/libopenscap.so";
+	} else {
+		$libopenscap_path = "$version/lib/libopenscap.so";
+	}
+	system ("abi-dumper $libopenscap_path -public-headers $version/include/openscap -lver $version -o $version.dump");
 }
 
 download($old);
@@ -51,8 +63,9 @@ download($new);
 build($new);
 dump_abi($new);
 
+system("pwd");
 system("abi-compliance-checker -lib openscap -old $old.dump -new $new.dump");
-system("cp compat_reports/openscap/${old}_to_${new}/compat_report.html reports/${old}_${new}.html");
+system("cp compat_reports/openscap/${old}_to_${new}/compat_report.html reports/${old}_${new}.html") and die "Can't find compat_reports/openscap/${old}_to_${new}/compat_report.html";
 system("firefox reports/${old}_${new}.html");
 system("git add reports/${old}_${new}.html");
 print "\nPlease commit and push the latest report to the repository\n";
